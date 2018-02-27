31f3150170e3ab2d895da0fdb57a9c3bc8002c66
Administrator <gaoqihua@empowerwin.com>
2018-02-27 10:03:39 +0800

    灵活配置

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Maven Project .
 与 mybatisMappergener 逆向原理相同都是利用mybatis官方的generator包。
 
 与generateCode 逆向原理不同，利用freemaker逆向生成。
+
+步骤：
+1. generator.properties中配置数据库
+
+2. 配置包名（package）、表名（tableName）、模块名（domainObjectName）
+   1. 注:schema：在mysql中就是DB name，在pgsql中就是模式名称
+
+3. 运行GeneratorMain.main方法即可
--- src/main/java/com/gao/GeneratorMain.java
+++ src/main/java/com/gao/GeneratorMain.java
@@ -8,45 +8,63 @@ import org.mybatis.generator.internal.DefaultShellCallback;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
-public class GeneratorMain {  
-    public static void main(String[] args) {  
-        List<String> warnings = new ArrayList<String>();  
-        boolean overwrite = true;
-        String filename=GeneratorMain.class.getClassLoader().getResource("generatorConfig.xml").getFile();
-        // msyql
-        String classPathEntry3=GeneratorMain.class.getClassLoader().getResource("lib/mysql-connector-java-5.1.38.jar").getFile();
-        // postgresql
-        String classPathEntry=GeneratorMain.class.getClassLoader().getResource("lib/postgresql-9.4.1209.jar").getFile();
+public class GeneratorMain {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        InputStream in;
+        try {
+            in = GeneratorMain.class.getClassLoader().getResourceAsStream("generator.properties");
+            props.load(in);
+            File projectDir = new File(props.getProperty("project"));
+            File resourceDir = new File(props.getProperty("resource"));
+            if(!projectDir.exists()){
+                projectDir.mkdirs();
+            }
+            if(!resourceDir.exists()){
+                resourceDir.mkdirs();
+            }
+            List<String> warnings = new ArrayList<String>();
+            boolean overwrite = true;
+            String filename = GeneratorMain.class.getClassLoader().getResource("generatorConfig.xml").getFile();
+            // msyql
+            String classPathEntry3 = GeneratorMain.class.getClassLoader().getResource("lib/mysql-connector-java-5.1.38.jar").getFile();
+            // postgresql
+            String classPathEntry = GeneratorMain.class.getClassLoader().getResource("lib/postgresql-9.4.1209.jar").getFile();
 
-        //读取配置文件
-        File configFile = new File(filename);
-        ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config;
-        try {  
-            config = cp.parseConfiguration(configFile);  
-  
-            DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-            MyBatisGenerator myBatisGenerator;
+            //读取配置文件
+            File configFile = new File(filename);
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config;
             try {
-                config.addClasspathEntry(classPathEntry);
-                myBatisGenerator = new MyBatisGenerator(config, callback,
-                        warnings);
-                myBatisGenerator.generate(null);  
-                //打印结果
-                for(String str : warnings){  
-                    System.out.println(str);  
-                }  
-            } catch (Exception e) {  
-                e.printStackTrace();  
-            }  
-  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        } catch (XMLParserException e) {
-            e.printStackTrace();  
-        }  
-    }  
-}  
+                config = cp.parseConfiguration(configFile);
+
+                DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+                MyBatisGenerator myBatisGenerator;
+                try {
+                    config.addClasspathEntry(classPathEntry);
+                    myBatisGenerator = new MyBatisGenerator(config, callback,
+                            warnings);
+                    myBatisGenerator.generate(null);
+                    //打印结果
+                    for (String str : warnings) {
+                        System.out.println(str);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (XMLParserException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
