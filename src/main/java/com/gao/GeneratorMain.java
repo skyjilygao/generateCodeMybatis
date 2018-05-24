75e8524bd7e63997843588eb774aed7ebf5471f5
Administrator <gaoqihua@empowerwin.com>
2018-05-24 12:19:37 +0800

    灵活配置

--- src/main/java/com/gao/GeneratorMain.java
+++ src/main/java/com/gao/GeneratorMain.java
@@ -13,6 +13,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * 主文件，配置generator.properties文件后，直接运行main方法即可
+ * <p> generator.properties配置
+ * @author skyjilygao
+ */
 public class GeneratorMain {
     public static void main(String[] args) {
         Properties props = new Properties();
@@ -20,13 +25,26 @@ public class GeneratorMain {
         try {
             in = GeneratorMain.class.getClassLoader().getResourceAsStream("generator.properties");
             props.load(in);
+            Boolean gnc = Boolean.parseBoolean(props.getProperty("generateNewCode"));
             File projectDir = new File(props.getProperty("project"));
             File resourceDir = new File(props.getProperty("resource"));
             if(!projectDir.exists()){
                 projectDir.mkdirs();
+            }else {
+                if(gnc){
+                    System.out.println("del....");
+                    delFolder(props.getProperty("project"));
+                    projectDir.delete();
+                    projectDir.mkdirs();
+                }
             }
             if(!resourceDir.exists()){
                 resourceDir.mkdirs();
+            }else {
+                if(gnc){
+                    resourceDir.delete();
+                    resourceDir.mkdirs();
+                }
             }
             List<String> warnings = new ArrayList<String>();
             boolean overwrite = true;
@@ -66,5 +84,49 @@ public class GeneratorMain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    //删除文件夹
+    public static void delFolder(String folderPath) {
+        try {
+            delAllFile(folderPath); //删除完里面所有内容
+            String filePath = folderPath;
+            filePath = filePath.toString();
+            java.io.File myFilePath = new java.io.File(filePath);
+            myFilePath.delete(); //删除空文件夹
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+//    删除指定文件夹下的所有文件
+
+    public static boolean delAllFile(String path) {
+        boolean flag = false;
+        File file = new File(path);
+        if (!file.exists()) {
+            return flag;
+        }
+        if (!file.isDirectory()) {
+            return flag;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                delAllFile(path + "/" + tempList[i]);//先删除文件夹里面的文件
+                delFolder(path + "/" + tempList[i]);//再删除空文件夹
+                flag = true;
+            }
+        }
+        return flag;
     }
 }
