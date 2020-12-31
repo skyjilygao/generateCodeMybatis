79f5644ec51916cc6f1589f04ed97ddefe4200ba
Administrator <gaoqihua@empowerwin.com>
2020-12-31 17:01:49 +0800

    优化批量插入

--- src/main/java/com/gao/plugins/batchInsert/BatchInsertMethodGeneratorXmlMapper.java
+++ src/main/java/com/gao/plugins/batchInsert/BatchInsertMethodGeneratorXmlMapper.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 /**
  * 生成自定义方法xml：selectBySelective
+ *
  * @author skyjilygao
  * @since 20181106
  */
@@ -21,6 +22,7 @@ public class BatchInsertMethodGeneratorXmlMapper extends AbstractXmlElementGener
     public void setMethodName(String methodName) {
         this.methodName = methodName;
     }
+
     public BatchInsertMethodGeneratorXmlMapper() {
     }
 
@@ -41,9 +43,7 @@ public class BatchInsertMethodGeneratorXmlMapper extends AbstractXmlElementGener
         String tableName = introspectedTable.getFullyQualifiedTableNameAtRuntime();
         this.context.getCommentGenerator().addComment(answer);
         StringBuilder sb = new StringBuilder();
-        sb.append("insert into ").append(tableName)
-        .append(" ( ")
-        ;
+        sb.append("insert into ").append(tableName).append(" ( ");
         answer.addElement(new TextElement(sb.toString()));
 
         answer.addElement(this.getBaseColumnListElement());
@@ -51,32 +51,50 @@ public class BatchInsertMethodGeneratorXmlMapper extends AbstractXmlElementGener
 
         // forear开始
         sb.setLength(0);
-        sb.append("values <foreach collection=\"list\" item=\"item\" index=\"index\" separator=\",\">");
+        sb.append("values");
         answer.addElement(new TextElement(sb.toString()));
+
+        XmlElement foreachElm = new XmlElement("foreach");
+        foreachElm.addAttribute(new Attribute("collection", "list"));
+        foreachElm.addAttribute(new Attribute("item", "item"));
+        foreachElm.addAttribute(new Attribute("index", "index"));
+        foreachElm.addAttribute(new Attribute("separator", ","));
+        answer.addElement(foreachElm);
+
         // 值
         sb.setLength(0);
-        // 对应主键id 的值为null
-        sb.append("(null");
-        Iterator i$ = this.introspectedTable.getAllColumns().iterator();
-        StringBuffer colBuf = new StringBuffer();
-        while(i$.hasNext()) {
-            IntrospectedColumn introspectedColumn = (IntrospectedColumn)i$.next();
+        foreachElm.addElement(new TextElement("("));
+        List<IntrospectedColumn> allColumns = this.introspectedTable.getAllColumns();
+        int size = allColumns.size();
+        int a = 0;
+        int t = 0;
+        for (int i = 0; i < size; i++) {
+            IntrospectedColumn introspectedColumn = allColumns.get(i);
             String col = introspectedColumn.getJavaProperty();
             if ("id".equalsIgnoreCase(col)) {
                 System.out.println("忽略" + col);
-                colBuf.append("null");
-                continue;
-            }
-            colBuf.append(" ,#{item.").append(col).append("}");
-//            sb.setLength(0);
-            sb.append(", #{item.").append(col).append("}");
+                // 对应主键id 的值为null
+                sb.append("null");
+            } else {
+                if (sb.length() > 0) {
+                    sb.append(",");
+                }
+                sb.append("#{item.").append(col).append("}");
+                if ((i + 1) % 5 == 0 || (i + 1)==size) {
+                    if (size > (i + 1)) {
+                        sb.append(",");
+                    }
+                    foreachElm.addElement(new TextElement(sb.toString()));
+                    sb.setLength(0);
+                }
 
+            }
         }
-        sb.append(")");
+
+//        forearchElm.addElement(new TextElement(sb.toString()));
+        foreachElm.addElement(new TextElement(")"));
 //        answer.addElement(new TextElement(sb.toString()));
         // foreach结束
-        sb.append("</foreach>");
-        answer.addElement(new TextElement(sb.toString()));
 /*
 
 
