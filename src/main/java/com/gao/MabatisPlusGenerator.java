ab5b9e02296b0e0e0d50284495193c9583fcbab3
skyjilygao <gaoqihua@empowerwin.com>
2023-09-08 16:09:12 +0800

    tableField

--- src/main/java/com/gao/MabatisPlusGenerator.java
+++ src/main/java/com/gao/MabatisPlusGenerator.java
@@ -30,16 +30,42 @@ public class MabatisPlusGenerator {
         // 1. 是否要删除之前生成的文件
         clearOld = true;
         // 2. 配置包名
-        basePackage = "com.sky.shop";
+        basePackage = "com.powerwin.meta.data.center";
         // 3. 类的java doc 作者
         authorName = "skyjilygao";
 
         // 4. 添加表名
-        tables.add("specifications");
+        tables.add("ali_fb_excel_campagin_dwd");
+        tables.add("ali_lahuo_fb_adset_dwd");
+        tables.add("ali_laxin_fb_campaign_dwd");
+        tables.add("ali_laxinios_fb_campaign_dwd");
+        tables.add("ali_store_fb_campaign_dwd");
+        tables.add("ali_xinghe_fb_account_dwd");
+        tables.add("ali_xinghe_fb_campaign_dwd");
+        tables.add("ali_xinghe_gg_account_dwd");
+        tables.add("ali_xinghe_gg_campaign_dwd");
+        tables.add("fb_account_dwd");
+        tables.add("fb_adset_dwd");
+        tables.add("fb_campaign_dwd");
+        tables.add("google_account_dws");
+        tables.add("google_campaign_dws");
+        tables.add("meta_account_dws");
+        tables.add("meta_adset_dws");
+        tables.add("meta_campaign_dws");
+
 
         // 5. 配置数据库
         DbDto dbDto;
-        dbDto = new DbDto(DbType.MYSQL, "192.168.8.15", 3306, "test", "test", "123456");
+        dbDto = new DbDto(DbType.MYSQL, "192.168.88.31 ", 31000, "pw_big_data_fat", "root", "");
+//        dbDto = new DbDto(DbType.MYSQL, "8.214.46.129", 3306, "powerwinmeta", "powerwinmeta", "powerwinmeta123");
+//        dbDto = new DbDto(DbType.MYSQL, "47.88.255.121", 3306, "ae_month_data", "user-ae-mon", "kjd2020ha*nchsl");
+//        dbDto = new DbDto(DbType.MYSQL, "47.88.255.121", 3306, "crm_master", "usercrmcrm", "master2020#!BV9mn");
+//        dbDto = new DbDto(DbType.MYSQL, "47.88.157.22", 3306, "crm_master", "user-crm", "cjshOMNIJkcoj");
+//        dbDto = new DbDto(DbType.MYSQL, "47.241.48.232", 3306, "pw_data_fb", "pw_data_fb", "pw_data_fb1qaz2ws#");
+//        dbDto = new DbDto(DbType.MYSQL, "192.168.88.89", 3306, "dws_powerwin_fb_data_7after", "user-admin", "ceshi");
+//        dbDto = new DbDto(DbType.MYSQL, "192.168.88.150", 3306, "adorado_platform", "adorado_platform", "adorado_platform1qaz2wsx");
+//        dbDto = new DbDto(DbType.MYSQL, "192.168.88.155", 3306, "sww_auto_db", "sww_auto_db", "sww_auto_db123%");
+//        dbDto = new DbDto("192.168.88.155", 3306, "big_cms", "user_big_cms", "user_big_cms123#");
 
         // 6. 执行
         execute(dbDto);
@@ -114,6 +140,8 @@ public class MabatisPlusGenerator {
                         // .setEntityBuilderModel(true)
                         // 【实体】是否为lombok模型（默认 false）<a href="https://projectlombok.org/">document</a>
                         .setEntityLombokModel(true)
+                // 实体类字段加@TableField注解，针对new_buyer_30d_num生成newBuyer30dNum后在查询反向解析成new_buyer30d_num导致找不到字段
+                        .setEntityTableFieldAnnotationEnable(true)
                 // Boolean类型字段是否移除is前缀处理
                 // .setEntityBooleanColumnRemoveIsPrefix(true)
                 // .setRestControllerStyle(true)
